e43e55ef77a66ee3034f20e28b0b231cdaeb4b9d
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-06-03 04:00:19 +0700

    Centralize mongoose configuration

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+
+const mongoose = require("mongoose")
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || `mongodb://localhost:27017/database_name`
+
+mongoose.set("useCreateIndex", true)
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+module.exports = mongoose
